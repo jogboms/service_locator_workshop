f04488f318ade2119916462def45c066dc7a515f
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2023-09-10 08:42:44 +0200

    Implementation

--- lib/service_locator_workshop.dart
+++ lib/service_locator_workshop.dart
@@ -3,22 +3,45 @@ import 'package:meta/meta.dart';
 typedef RegistryFactory = T Function<T>();
 
 class Registry {
+  final _instances = Expando<Object>();
+
   void set<T>(T instance) {
-    throw UnimplementedError();
+    switch (_instances[T]) {
+      case final Object _:
+        throw AssertionError('Already set: $T');
+      case _:
+        replace(instance);
+    }
   }
 
   T get<T>() {
-    throw UnimplementedError();
+    switch (_instances[T]) {
+      case final T Function(void) factory:
+        final instance = factory(null);
+        replace(instance);
+        return instance;
+      case final T Function() factory:
+        return factory();
+      case final T instance:
+        return instance;
+      case _:
+        throw ArgumentError('Not set: $T');
+    }
   }
 
   void factory<T>(T Function(RegistryFactory) fn, {@visibleForTesting bool lazy = false}) {
-    throw UnimplementedError();
+    switch (_instances[T]) {
+      case final Object _:
+        throw AssertionError('Already set: $T');
+      case _:
+        _instances[T] = lazy ? (_) => fn(get) : () => fn(get);
+    }
   }
 
   void lazy<T>(T Function(RegistryFactory) fn) => factory(fn, lazy: true);
 
   @visibleForTesting
   void replace<T>(T instance) {
-    throw UnimplementedError();
+    _instances[T] = instance as Object;
   }
 }
